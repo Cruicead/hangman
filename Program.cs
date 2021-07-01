5d4b59febc3b5a7df19c367749e02b45c8d0b57f
Cruicead <70847650+Cruicead@users.noreply.github.com>
2021-07-02 01:38:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Hangman
+{
+    class Hangman
+    {
+        public static void Main()
+        {
+            string path = @"countries_and_capitals.txt";
+
+            var countries =
+            from line in File.ReadAllLines(path)
+            let columns = line.Split('|')
+            select new
+            {
+                country = columns[0].Trim(),
+                capital = columns[1].Trim()
+            };
+
+            Random rnd = new Random();
+
+            var result = countries.ElementAt(rnd.Next() %countries.Count());
+
+            string regex = "\\S";
+            string puzzle = Regex.Replace(result.capital, regex, "_");
+            string letters = "";
+            string letter = "";
+            int lives = 5;
+            bool victory = false;
+
+            while (true)
+            {
+                Console.WriteLine(puzzle + "   lives: " + lives + "   guessed letters: " + letters);
+                Console.WriteLine("Write \"lt\" if you want to guess a letter, or \"wd\" if you want to guess the word.");
+                switch (Console.ReadLine())
+                {
+                    case "lt":
+                        letter = Console.ReadLine();
+                        letters += letter;
+                        regex = "[^\\s" + letters + letters.ToUpper() + "]";
+                        if (!result.capital.Contains(letter) && !result.capital.Contains(letter.ToUpper()))
+                        {
+                            lives--;
+                        }
+                        break;
+                    case "wd":
+                        letter = Console.ReadLine();
+                        if (!result.capital.Contains(letter))
+                        {
+                            lives-=2;
+                        }
+                        else
+                        {
+                            victory = true;
+                        }
+                        break;
+                }
+                puzzle = Regex.Replace(result.capital, regex, "_");
+                if (lives < 1)
+                {
+                    Console.WriteLine("Game over!");
+                    break;
+                }
+                if (victory || puzzle.Equals(result.capital))
+                {
+                    Console.WriteLine("Correct, it's " + result.capital + ". Congratulations, you won!");
+                    break;
+                }
+            }
+        }
+    }
+}
